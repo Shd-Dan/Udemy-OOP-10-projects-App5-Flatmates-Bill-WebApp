92708a88e8710a3c8b212380ffeda8444e154622
Shaken-Dan Bolatuly <shd.cs.ch@gmail.com>
2023-05-15 15:19:56 +0600

    Added methuds

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask
+
+app = Flask(__name__)
 
 
-class HomePage:
+class HomePage(MethodView):
+
+    def get(self):
+        return 'Home Page it is'
+
+
+class BillFormPage(MethodView):
+
+    def get(self):
+        return 'I am the bill page'
+
+
+class ResultsPage(MethodView):
     pass
 
 
-class BillFormPage:
+class BillForm(Form):
     pass
 
 
-class ResultsPage:
-    pass
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
 
-
-class BillForm:
-    pass
+app.run()
